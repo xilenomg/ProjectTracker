fb4d83eb43e8265ec12dcf625f782236e50ce2c8
Luis Perez <xilenomg@gmail.com>
2015-04-02 11:23:17 -0300

    Starting Angular setting up

--- public/assets/javascript/app/app.js
+++ public/assets/javascript/app/app.js
@@ -1,1 +1,16 @@
-console.log("OLA");
+var projectTrackerApp = angular.module('projectTrackerApp', [
+	'ng',
+	'ngRoute',
+	'ngResource',
+	'ngMessages',
+	'ngCookies',
+	'angularModalService'
+]);
+
+projectTrackerApp.config([ '$routeProvider', function($routeProvider) {
+	$routeProvider.when('/', {
+		title: 'Welcome to ProjectTracker',
+		templateUrl : 'views/pages/login.html',
+		controller : 'LoginController'
+	});
+}]);
---
+++ public/assets/javascript/app/controllers/header-controller.js
@@ -0,0 +1,3 @@
+projectTrackerApp.controller('HeaderController', [function(){
+	console.log("aloalo");
+}]);
